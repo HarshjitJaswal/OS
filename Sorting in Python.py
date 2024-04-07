efe39fb8640e754546c5b412a7aad2fa3b0da358
HarshjitJaswal <163729883+HarshjitJaswal@users.noreply.github.com>
2024-04-07 14:07:46 +0530

    Add files via upload

---
+++ Sorting in Python.py
@@ -0,0 +1,12 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+a=[30,20,40,50,10,60]
+print(f"Unsorted Array: {a}")
+a.sort()
+print(f"Sorted Array: {a}")
