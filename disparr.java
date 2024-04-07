1607185fd6e39379d7866a998cfb7ff189bb8749
HarshjitJaswal <163729883+HarshjitJaswal@users.noreply.github.com>
2024-04-07 15:08:37 +0530

    Add files via upload

---
+++ disparr.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class disparr {
+    public static void main(String args[]) {
+        int[] arr={90,2,3,4,45,6,565,75};
+        for (int i=0;i<8;i++){
+            System.out.printf("%d  ",arr[i]);
+        }
+    }
+}
