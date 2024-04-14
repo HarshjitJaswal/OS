26013047e3a6ff3659d4302e41b79e766b228e5b
HarshjitJaswal <163729883+HarshjitJaswal@users.noreply.github.com>
2024-04-14 16:40:15 +0530

    Create C1.cpp

---
+++ CLASS/C1.cpp
@@ -0,0 +1,13 @@
+
+#include "iostream"
+using namespace std;
+class myclass{
+    public:
+    void hello(){
+        cout<<"Hello World";
+    }
+};
+int main(){
+    myclass obj;
+    obj.hello();
+}
