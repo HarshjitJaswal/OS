361d2a228aeb9e98046dc579fbcbe0ad215cb31e
HarshjitJaswal <163729883+HarshjitJaswal@users.noreply.github.com>
2024-04-14 16:44:01 +0530

    Create C3.cpp

---
+++ CLASS/C3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    void sound() {
+        cout << "Animal makes a sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() {
+        cout << "Dog barks" << endl;
+    }
+};
+
+int main() {
+    Dog myDog;
+    myDog.sound(); // Output: Dog barks
+    return 0;
+}
