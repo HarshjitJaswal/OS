f118f44c686ba4922a656348dcd594dec27fab9a
HarshjitJaswal <163729883+HarshjitJaswal@users.noreply.github.com>
2024-04-28 21:09:49 +0530

    Add files via upload

---
+++ Search element in vector.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int main() {
+    vector<int> vec = {2, 4, 6, 8, 10};
+    int Element = 6;
+    int c=0;
+
+    for (int i = 0; i < vec.size(); ++i) {
+        if (vec[i] ==Element) {
+            c++;
+            break;
+        }
+    }
+
+    if (c!=0) {
+        cout << "Element found in the vector." << std::endl;
+    } else {
+        cout << "Element not found in the vector." << std::endl;
+    }
+
+    return 0;
+}
